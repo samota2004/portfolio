d39f93a5402658a275bef7011341ccbe50571ff9
Priyanka Samota <priyankasamota946@gmail.com>
2026-03-15 00:48:49 +0530

    Update Project.js

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,11 +1,41 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  tech: [String],
-  live: String,
-  github: String,
-});
+const projectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    tech: [
+      {
+        type: String,
+      },
+    ],
+
+    live: {
+      type: String,
+      default: "",
+    },
+
+    github: {
+      type: String,
+      default: "",
+    },
+
+    image: {
+      type: String, // image URL
+      default: "",
+    },
+  },
+  {
+    timestamps: true, // createdAt & updatedAt
+  }
+);
 
 module.exports = mongoose.model("Project", projectSchema);
