6f6fa92b027c515b4e63c690d5b7949dc3847e20
Priyanka Samota <priyankasamota946@gmail.com>
2026-03-15 13:54:39 +0530

    Update Project.js

--- server/models/Project.js
+++ server/models/Project.js
@@ -5,36 +5,39 @@ const projectSchema = new mongoose.Schema(
     title: {
       type: String,
       required: true,
+      trim: true
     },
 
     description: {
       type: String,
       required: true,
+      trim: true
     },
 
     tech: [
       {
         type: String,
-      },
+        trim: true
+      }
     ],
 
     live: {
       type: String,
-      default: "",
+      default: ""
     },
 
     github: {
       type: String,
-      default: "",
+      default: ""
     },
 
     image: {
-      type: String, // image URL
-      default: "",
-    },
+      type: String,
+      default: ""
+    }
   },
   {
-    timestamps: true, // createdAt & updatedAt
+    timestamps: true
   }
 );
 
